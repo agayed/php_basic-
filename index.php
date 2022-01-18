be62464b153600c9871cccca527ca292a8c320ca
عبدالجيد محمد عبدالجيد <40858684+agayed@users.noreply.github.com>
2022-01-18 11:39:43 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        nav{
+            font-size:100px;
+        }
+        h1{
+            background-color: aqua;
+        }
+        h2{
+            background-color:blue;
+            color:chartreuse;
+        }
+        ul{
+            flex-direction: column;
+            background-color:brown;
+            display: flex;
+            flex: 1 200px;
+  align-items: center;
+  justify-content: space-around;
+        }
+        ul:nth-of-type(3) {
+  flex: 2 200px;
+}
+    </style>
+</head>
+
+<body>
+    <nav>
+    <ul>
+        <h1>المحتويات</h1>
+        <li>
+            <ul>
+                <h1>PHP مقدمة</h1>
+                <li>
+                    <h2>ما يجب أن تعرفه بالفعل</h2>
+                </li>
+                <li>
+                    <h2>ما هو PHP؟</h2>
+                </li>
+                <li>
+                    <h2>ما هو ملف PHP؟</h2>
+                </li>
+                <li>
+                    <h2>ماذا يمكن أن بي إتش بي تفعل؟</h2>
+                </li>
+                <li>
+                    <h2>لماذا PHP؟</h2>
+                </li>
+                <li>
+                    <h2>ما الجديد في PHP 7</h2>
+                </li>
+
+
+            </ul>
+        </li>
+        <li>
+            <ul>
+                <h1>PHP تثبيت</h1>
+                <li>
+                    <h2>ما الذي أحتاجه؟</h2>
+                </li>
+                <li>
+                    <h2>PHP مترجم على الانترنت / محرر</h2>
+                </li>
+            </ul>
+        </li>
+
+        <li>
+            <ul>
+                <h1>بناء جملة PHP</h1>
+                <li>
+                    <h2>بناء جملة PHP الأساسي</h2>
+                </li>
+                <li>
+                    <h2>حساسية حالة PHP</h2>
+                </li>
+            </ul>
+        </li>
+
+
+    </ul>
+    </nav>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+
+</html>
